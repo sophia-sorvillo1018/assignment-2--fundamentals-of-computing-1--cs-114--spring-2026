3937c4cac464967b275d3b700c854728a89b24cf
sophia-sorvillo1018 <sophialily1018@gmail.com>
2026-03-04 22:00:11 -0500

    Finished coin flip program

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,20 @@
 public class CountFlips {
   public static void main(String[] args) {
+    int headsCount = 0;
+    int tailsCount = 0;
+
+    Coin coin = new Coin();
+
+    for (int i = 0; i < 100; i++ ){
+      coin.flip();
+      if (coin.isHeads()) {
+        headsCount++;
+      } else {
+        tailsCount++;
+      }
+    }
+
+    System.out.println("Number of heads: " + headsCount);
+    System.out.println("Number of tails: " + tailsCount);
   }
 }
