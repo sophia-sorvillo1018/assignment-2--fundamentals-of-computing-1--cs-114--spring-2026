6a658bb8434b90e83153e4e14fe29da58286f60f
sophia-sorvillo1018 <sophialily1018@gmail.com>
2026-03-10 19:59:51 -0400

    Finished 100 Bottles of Beer program

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
+
 public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter a number of verses to print: ");
+    int numOfVerses = input.nextInt();
+
+    int bottlesOfBeer = 100;
+
+    for (int i = 0; i < numOfVerses; i++ ){
+      System.out.println(bottlesOfBeer + " bottles of beer on the wall");
+      System.out.println(bottlesOfBeer + " bottles of beer");
+      System.out.println("If one of those bottles should happen to fall");
+      --bottlesOfBeer;
+      System.out.println(bottlesOfBeer + " bottles of beer on the wall");
+      System.out.println();
+
+    }
+
+    input.close();
   }
 }
